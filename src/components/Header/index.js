a20c5ffedd74675c52f13e91eb7c7c8c70dcca5e
Leonardo Macedo <lmacedo.freire@gmail.com>
2021-12-01 23:35:00 -0300

    estilo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,56 +1,113 @@
-import { useState, useEffect } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import {
-  Grid,
-  GridItem,
-  Container,
-  FormControl,
-  FormLabel,
-  Select
-}  from '@chakra-ui/react'
-import Logo from '../../assets/logo.jpeg';
+import {useState, useEffect} from 'react';
+import {Link, useNavigate} from 'react-router-dom'
+import{
+    Grid,
+    GridItem,
+    Container,
+    FormControl,
+    FormLabel,
+    Select,
+    Flex
+} from '@chakra-ui/react'
+import Logo from '../../assets/logo.png';
 
 import api from '../../services/api'
 
 
 const Header = () => {
-  const [main, setMain] = useState([]);
-  const navigate = useNavigate();
-  useEffect(() => {
-    api.get('categories').then(
-      res => {
-        setMain(res.data)
-      }
-    )
-  }, [])
+    const [main, setMain] = useState([]);
+    const navigate = useNavigate();
+    useEffect(()=>{
+        api.get('categories').then(
+            res => {
+                setMain(res.data)
+            }
+        )
+    }, []);
 
-  const handleCategory = (e) => {
-    navigate(`/categories/${e.target.value}`)
-  }
 
-  return(
-    <nav>
-      <Container maxW="container.xl">
-        <Grid templateColumns="repeat(5, 1fr)" gap={10}>
-          <GridItem colStart={1}>
+    const handleCategory = (e) => {
+        navigate(`/categories/${e.target.value}`)
+    }
+    /*return(
+        <nav>
+             <Container maxW="container.xl">
+
+            <Grid templateColums="repeat(5, 1fr)" gap={10}>
+
+                <GridItem colStart={1}>
             <Link to='/'>
-              <img src={Logo} className="logo" alt="Logo" />
+            <img src={Logo} className="logo" alt="Logo" />
             </Link>
-          </GridItem>
-          <GridItem colStart={6} colEnd={12} h="150px">
+            </GridItem>
+            
+            <GridItem colStart={6} colEnd={12} h="150px" > 
+            
             <FormControl>
-              <FormLabel>Selecione a categoria de sua piada</FormLabel>
-              <Select onChange={handleCategory}>
+                <FormLabel>Selecione a categoria de sua piada</FormLabel>
+                <Select onChange={handleCategory}>
                 {main?.map( (item, index) => (
-                  <option key={index} value={item}> {item} </option>
+                    <option key={index} value={item} > {item} </option>
+                    
                 ))}
-              </Select>
+                </Select>
             </FormControl>
-          </GridItem>
-        </Grid>
-      </Container>
+           
+            </GridItem>
+            
+
+            </Grid>
+
+           </Container>
     </nav>
-  )
+
+    
+
+    )*/
+
+
+
+    return(
+        <nav>
+             <Container maxW="container.xl">
+
+            <Flex justify="space-between" align="center">
+
+                
+            <Link to='/'>
+            <img src={Logo} className="logo" alt="Logo" />
+            </Link>
+            
+            
+            
+            <div className="seletor_categorias">
+            <FormControl>
+                <FormLabel> <span className="label_seletor">Selecione a categoria de sua piada</span></FormLabel>
+                <Select onChange={handleCategory}  bg='#f98484' color='white' >
+                {main?.map( (item, index) => (
+                    <option key={index} value={item} > {item} </option>
+                    
+                ))}
+                </Select>
+            </FormControl>
+            </div>
+           
+            
+            
+
+            </Flex>
+
+           </Container>
+    </nav>
+
+    )
+
+
+
+
+
+
 }
+
 
 export default Header;
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,83 +1,124 @@
 import React, {useEffect, useState} from 'react';
-
 import {
   FormLabel,
   Input,
-  Button
+  Button,
 } from '@chakra-ui/react'
 
 import api from '../../services/api';
 
-import Loader from '../../assets/loader.gif'
+import Loader from '../../assets/loader.gif';
 
 const App = () => {
+  console.log("Hommmme");
   const [data, setData] = useState({});
-  const [allJokes, setAllJokes] = useState({});
-  const [isSearch, setIsSearch] = useState(false);
+  console.log(typeof data);// data é do tipo objeto
+  console.log(data);
+  const [allJokes, setAllJokes]= useState({});
+  console.log(typeof allJokes);
+  const [isSearch, setIsSearch]= useState(false);
+  console.log(typeof isSearch);
   const [isLoad, setIsLoad] = useState(false);
-  const [searchJoke, setSearchJoke] = useState('');
+  const [searchJoke, setSearchJoke] = useState('');//searchJoke é uma string que vai armazenar o termo da piada a ser pesquisada
+  console.log(typeof searchJoke);
 
   useEffect(() => {
-    setIsLoad(true)
+    setIsLoad(true);//torno isLoad true
     api.get('random').then(
       response => {
+        //Aqui atrubui uma piada randomica da api para data que é um objeto
         setData(response.data)
       }
+      
     )
     .catch( e => console.error(e))
-    .finally( () => setTimeout(() => {
+    .finally(
+      () => setTimeout(() => {
       setIsLoad(false)
-    }, 2500))
-  }, [])
+    }, 
+    2000)// esse e o tempo que isLoader vai ser true (2500 é o que tava)
+    )
+  }, []);
+  
+  
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = (e)=> {
+    e.preventDefault();//pra quando submeter o form nao reload a página
+  
     setIsLoad(true)
+    console.log(searchJoke);
+    console.log(` search?query=${searchJoke}`);
     api.get(`search?query=${searchJoke}`).then(
       res => {
         setIsSearch(true)
-        setAllJokes(res.data)
+       
+        setAllJokes(res.data)//vai ficar a piada pesquisada pelo termo
+        console.log(allJokes);
       }
-    )
-    .catch( err => console.error(err))
-    .finally( () => setIsLoad(false))
+    ).catch( err => console.log(err))
+    .finally(( ) => setIsLoad(false))
+
+
   }
 
+  //enquanto isLoader for true vai passar um gif (Loader) para renderizacao
   if(isLoad) {
+    console.log("Loaaaader");
     return(
       <div className="loader">
-        <img src={Loader} alt="Loader" />
+        <img src={Loader} alt="Loader"/>
       </div>
     )
   }
 
+  
   return(
     <div className="home-component">
-      <h1>Joke</h1>
-      <div>
-        <form onSubmit={handleSubmit}>
-          <FormLabel>Pesquise sua piada</FormLabel>
-          <Input type="text" onChange={e => setSearchJoke(e.target.value)} />
-          <Button type="submit">Pesquisar</Button>
-        </form>
+      <div className="titulo">
+         <h1 > Chuck Norris Jokes</h1>
       </div>
-      { !isSearch ? (
+      
+      
+        <form onSubmit={handleSubmit}>
+          
+          <Input type="text" onChange={ e => setSearchJoke(e.target.value)}  className="entrada_pesquisa" placeholder='Pesquise sua piada' size='lg' />
+          <Button type='submit' className="botao_pesquisa" size='lg' variant='outline' colorScheme="black" >
+            Pesquisar
+          </Button>
+            
+
+        </form>
+      
+
+      {!isSearch ? (
         <div className="jokes">
-          <img src={data?.icon_url} alt={data?.value} />
-          <h3>{data?.value}</h3>
-        </div>
+        
+        <h3>{data?.value}</h3>
+      </div>
+
       ) : (
         <>
-          { allJokes?.result.map( (item, index) => (
-            <div key={index} className="jokes">
-              <img src={item?.icon_url} alt={item?.value} />
-              <h3>{item?.value}</h3>
-            </div>
-          ))}
+        { allJokes?.result.map(
+          (item, index) => (
+
+          <div key= {index} className="jokes">
+            
+            <h3>{item?.value}</h3>
+          </div>
+
+          )
+        )}
         </>
       )}
+      
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
+
+/*
+<img src={data?.icon_url} alt={data?.value}/>
+<img src={item?.icon_url} alt={item?.value}/>
+*/
