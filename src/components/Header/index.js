29ad8f30c4364e486233b529b76e047d25e50b94
Leonardo Macedo <lmacedo.freire@gmail.com>
2021-12-01 23:42:38 -0300

    estilo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,8 @@
 import {useState, useEffect} from 'react';
 import {Link, useNavigate} from 'react-router-dom'
 import{
-    Grid,
-    GridItem,
+    
+    
     Container,
     FormControl,
     FormLabel,
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {
-  FormLabel,
+  
   Input,
   Button,
 } from '@chakra-ui/react'
